c264e23ce2d6c96f173d761cbab03c4df2163940
ayush-2302 <ayushkumarakt@gmail.com>
2025-03-18 22:34:43 +0530

    first commit

--- ui/src/service/httpService.js
+++ ui/src/service/httpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://booking-app-x040.onrender.com/api",
 });
 
 export default httpService;
